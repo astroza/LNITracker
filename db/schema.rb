6834cf63c32a9c1fd14aee5205a1f83fa0d051f3
Felipe Astroza <felipe@astroza.cl>
2014-01-06 00:01:16 -0300

    geokit

--- db/schema.rb
+++ db/schema.rb
@@ -21,14 +21,6 @@ ActiveRecord::Schema.define(version: 20131226212355) do
     t.datetime "updated_at"
   end
 
-  create_table "busstops", force: true do |t|
-    t.string   "name"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "command_responses", force: true do |t|
     t.string   "response"
     t.integer  "device_id"
